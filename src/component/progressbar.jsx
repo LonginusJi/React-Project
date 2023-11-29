ea13a5fdb82d1abc73a7f97d93c0445512d4089e
Jerry <jiyijun@outlook.com>
2023-11-30 00:43:14 +0800

    Update hooks

--- src/component/progressbar.jsx
+++ src/component/progressbar.jsx
@@ -4,28 +4,28 @@ import PropTypes from 'prop-types';
 export default function ProgressBar(props) {
   const [length, setLength] = useState(0);
   const [background, setBackground] = useState('');
-  const progressBarRef = useRef();
-  progressBarRef.current = length;
+  const progressBar = useRef();
+  progressBar.current = length;
 
   useEffect(() => {
     const loader = setInterval(() => {
-      if (progressBarRef.current === 100) {
+      if (progressBar.current === 100) {
         props.loadFinished();
         clearInterval(loader);
         return;
       }
       // eslint-disable-next-line no-shadow
       setLength((length) => length + 1);
-      const secondStart = 180 - progressBarRef.current;
-      const thirdStart = 200 - progressBarRef.current;
+      const secondStart = 180 - progressBar.current;
+      const thirdStart = 200 - progressBar.current;
       setBackground(`linear-gradient(0.25turn, rgb(47, 11, 252) 0%, rgb(255, 25, 25) ${secondStart}%, rgb(18, 233, 57) ${thirdStart}%)`);
     }, 50);
-  }, [props]);
+  }, []);
 
   return (
     <div>
       <div className="progress-bar-container">
-        <div className="progress-bar" ref={progressBarRef} style={{ width: `${length}%`, background }} />
+        <div className="progress-bar" style={{ width: `${length}%`, background }} />
       </div>
       <span className="progress-label">
         Loading:
