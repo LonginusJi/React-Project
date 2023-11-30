6a3a6e6c3cba63f282d0b94f1a676546aa3adcfc
Jerry <jiyijun@outlook.com>
2023-11-30 10:09:49 +0800

    rect

--- src/pages/rect.jsx
+++ src/pages/rect.jsx
@@ -5,47 +5,29 @@ export default class Rect extends React.Component {
     super(props);
     this.state = {
       data: [10, 6, 8, 9, 12],
-      data1: 10,
-      data2: 6,
-      data3: 8,
-      data4: 9,
-      data5: 12,
     };
-  }
-
-  componentDidMount() {
-    const { data } = this.state;
-    const rects = document.querySelectorAll('.rect');
-    rects.forEach((el, i) => {
-      el.style.height = `${data[i] * 10}px`;
-    });
   }
 
   render() {
     const {
-      data1,
-      data2,
-      data3,
-      data4,
-      data5,
+      data,
     } = this.state;
     return (
       <div className="rect-container">
-        <div className="rect">
-          <span className="rect-label">{data1}</span>
-        </div>
-        <div className="rect">
-          <span className="rect-label">{data2}</span>
-        </div>
-        <div className="rect">
-          <span className="rect-label">{data3}</span>
-        </div>
-        <div className="rect">
-          <span className="rect-label">{data4}</span>
-        </div>
-        <div className="rect">
-          <span className="rect-label">{data5}</span>
-        </div>
+        {data.map((d, i) => {
+          const style = {
+            position: 'relative',
+            height: `${d * 10}px`,
+            width: '10px',
+            margin: '0 10px',
+            background: 'linear-gradient(red, green)',
+          };
+          return (
+            <div key={i} style={style}>
+              <span>{d}</span>
+            </div>
+          );
+        })}
       </div>
     );
   }
